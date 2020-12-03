fc67f4cda0866c348c82ff1960b1cfcf2a85bf35
DiamondYuan <admin@diamondyuan.com>
2020-12-04 00:50:23 +0800

    :sparkles: add types

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+export enum EFixtureTag {
+  skip = "skip",
+  only = "only",
+  default = "default",
+}
+
+export interface IFixture {
+  type: EFixtureTag;
+  tags: Set<string>;
+  readFile(path: string): Promise<string>;
+  readJSON<T = any>(path: string): Promise<T>;
+  writeJSON<T = any>(path: string, data: T): Promise<void>;
+  writeFile(path: string): Promise<void>;
+}
+
+export interface IFixtureManager {
+  getAll(): Promise<IFixture[]>;
+}
